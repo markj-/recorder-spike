ec94e4c79394f1b5b91e6557fd100767e6eee07f
Mark Jones <mark@thisismarkup.co.uk>
2019-11-29 12:24:53 +0000

    simplify

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,38 @@
 import Tone from "tone";
-import RecordRTC, { StereoAudioRecorder } from "recordrtc";
+// import RecordRTC, { StereoAudioRecorder } from "recordrtc";
 
 const main = async () => {
-  const audio = document.querySelector("audio");
-  const ctx  = Tone.context;
+  await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
+  // const ctx  = Tone.context;
   const mic = new Tone.UserMedia();
-  const dest = ctx.createMediaStreamDestination();
-  const recorder = RecordRTC(dest.stream, {
-    type: 'audio',
-    recorderType: StereoAudioRecorder,
-    numberOfAudioChannels: 1
-  });
   await mic.open();
-  mic.connect(dest);
-  recorder.startRecording();
-  setTimeout(() => {
-    recorder.stopRecording(() => {
-      const blob = recorder.getBlob();
-      if (audio) {
-        audio.src = URL.createObjectURL(blob);
-      }
-    });
-  }, 5000);
+  mic.toMaster();
+  // const dest = ctx.createMediaStreamDestination();
+  // const recorder = RecordRTC(dest.stream, {
+  //   type: 'audio',
+  //   recorderType: StereoAudioRecorder,
+  //   numberOfAudioChannels: 2,
+  //   checkForInactiveTracks: true,
+  //   bufferSize: 4096
+  // });
+  // await mic.open();
+  // mic.connect(dest);
+  // recorder.startRecording();
+  // setTimeout(() => {
+  //   recorder.stopRecording(() => {
+  //     const blob = recorder.getBlob();
+  //     const audio = document.createElement('audio');
+  //     if (audio) {
+  //       audio.src = URL.createObjectURL(blob);
+  //       audio.controls = true;
+  //       document.body.appendChild(audio);
+  //     }
+  //   });
+  // }, 5000);
 };
 
-document.documentElement.addEventListener('mousedown', main);
+const button = document.querySelector("button");
+
+if (button) {
+  button.addEventListener('click', main);
+}
