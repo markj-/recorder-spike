5d643d44d36f5bf96eb7d5b6f8a7d61e9aed77fd
Mark Jones <mark@thisismarkup.co.uk>
2019-11-29 13:02:41 +0000

    no tone

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,35 @@
-import Tone from "tone";
-// import RecordRTC, { StereoAudioRecorder } from "recordrtc";
+import RecordRTC, { StereoAudioRecorder } from "recordrtc";
 
+let mic: MediaStream;
 const request = async () => {
-  await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
+  mic = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
 };
+
 const main = async () => {
-  
-  // const ctx  = Tone.context;
-  const mic = new Tone.UserMedia();
-  await mic.open();
-  mic.toMaster();
-  // const dest = ctx.createMediaStreamDestination();
-  // const recorder = RecordRTC(dest.stream, {
-  //   type: 'audio',
-  //   recorderType: StereoAudioRecorder,
-  //   numberOfAudioChannels: 2,
-  //   checkForInactiveTracks: true,
-  //   bufferSize: 4096
-  // });
-  // await mic.open();
-  // mic.connect(dest);
-  // recorder.startRecording();
-  // setTimeout(() => {
-  //   recorder.stopRecording(() => {
-  //     const blob = recorder.getBlob();
-  //     const audio = document.createElement('audio');
-  //     if (audio) {
-  //       audio.src = URL.createObjectURL(blob);
-  //       audio.controls = true;
-  //       document.body.appendChild(audio);
-  //     }
-  //   });
-  // }, 5000);
+  // @ts-ignore
+  const ctx = new webkitAudioContext();
+  const src = ctx.createMediaStreamSource(mic);
+  const dest = ctx.createMediaStreamDestination();
+  const recorder = RecordRTC(dest.stream, {
+    type: 'audio',
+    recorderType: StereoAudioRecorder,
+    numberOfAudioChannels: 2,
+    checkForInactiveTracks: true,
+    bufferSize: 4096
+  });
+  src.connect(dest);
+  recorder.startRecording();
+  setTimeout(() => {
+    recorder.stopRecording(() => {
+      const blob = recorder.getBlob();
+      const audio = document.createElement('audio');
+      if (audio) {
+        audio.src = URL.createObjectURL(blob);
+        audio.controls = true;
+        document.body.appendChild(audio);
+      }
+    });
+  }, 5000);
 };
 
 const requestMic = document.getElementById("request-mic")
