d2f71c956bb2e8c1520d938cb072c196d7f8904b
Mark Jones <mark@thisismarkup.co.uk>
2019-12-02 12:32:00 +0000

    re-introduce recording

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,40 @@
 import Tone from "tone";
+import RecordRTC, { StereoAudioRecorder } from "recordrtc";
 
 const record = document.getElementById("record");
 const log = document.getElementById("log");
+const audio = document.querySelector("audio");
 
 const main = async () => {
   // @ts-ignore
   const mic = new Tone.UserMedia();
   const fft = new Tone.FFT();
+  const dest = Tone.context.createMediaStreamDestination();
+  mic.fan(fft, dest);
   Tone.start();
   await mic.open();
-  mic.connect(fft);
+
   setInterval(() => {
     if (log) {
-      log.innerText = fft.getValue();
-    }  
+      log.innerHTML = fft.getValue();
+    }
   }, 100);
+
+  let recorder = RecordRTC(dest.stream, {
+    type: 'audio',
+    recorderType: StereoAudioRecorder,
+    numberOfAudioChannels: 1
+  });
+  recorder.startRecording();
+
+  setTimeout(() => {
+    recorder.stopRecording(function() {
+      const blob = recorder.getBlob();
+      if (audio) {
+        audio.src = URL.createObjectURL(blob);
+      }
+    });
+  }, 3000);
 };
 
 if (record) {
