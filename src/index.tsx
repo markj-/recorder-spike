24b12ae7d0e7cc27f5a3b84db29d66d903202bc8
Mark Jones <mark@thisismarkup.co.uk>
2019-12-02 12:18:42 +0000

    modernize

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,20 @@
 import Tone from "tone";
 
-var record = document.getElementById("record");
-var log = document.getElementById("log");
+const record = document.getElementById("record");
+const log = document.getElementById("log");
 
-var main = function() {
+const main = async () => {
   // @ts-ignore
-  navigator.mediaDevices.getUserMedia({ audio: true, video: false })
-    .then(function() {
-      var mic = new Tone.UserMedia();
-      var fft = new Tone.FFT();
-      mic.open().then(function(){
-        mic.connect(fft);
-        setInterval(() => {
-          if (log) {
-            log.innerText = fft.getValue();
-          }  
-        }, 100);
-      });
-      Tone.start();
-    });
+  const mic = new Tone.UserMedia();
+  const fft = new Tone.FFT();
+  Tone.start();
+  await mic.open();
+  mic.connect(fft);
+  setInterval(() => {
+    if (log) {
+      log.innerText = fft.getValue();
+    }  
+  }, 100);
 };
 
 if (record) {
